c443ca5a983311b216591e816b29b232a23a180b
Andrew Reid East <andrew@andreweast.net>
2015-11-11 00:22:33 +0000

    Implement counting via nested loops

---
+++ NumberOfOnes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfOnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 11, end = 111, count = 0, i, place, currNum;
+            for (i = (start < end ? start : end); i <= (start > end ? start : end); i++)
+            {
+                //Console.WriteLine("i={0}", i);
+                currNum = i;
+                for (place = 10; currNum != 0; currNum /= 10, place *= 10)
+                {
+                    //Console.WriteLine("Place={0}, currNum%10={1}", place, currNum % 10);
+                    if (currNum % 10 == 1)
+                        count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
